795269c7992cfff1ffe7e4a6f7c81dd6952dd26e
michael Odz. Mtz <ordazmartinezmichael@gmail.com>
2022-05-05 18:07:58 +0000

    CI develop into qa

--- database/seeders/DocumentsSeeder.php
+++ database/seeders/DocumentsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Helpers\Dixa;
 use App\Models\Department;
 use App\Models\Document;
 use App\Models\DocumentType;
@@ -19,11 +20,18 @@ class DocumentsSeeder extends Seeder
     public function run()
     {
         $documentType = DocumentType::all();
+        $typeFolder = $documentType->where('name', Dixa::FOLDER)->first();
+        $typeFile = $documentType->where('name', Dixa::FILE)->first();
 
         //creo 10 documentos (carpetas y archivos) de cada jefe de departamento en el nivel raiz
         User::role('head of department')->get()
-        ->each(function ($user) use ($documentType) {
-            Document::factory()->count(10)
+        ->each(function ($user) use (
+            $documentType,
+            $typeFolder,
+            $typeFile
+        ) {
+            //level root
+            $documentsRoot = Document::factory()->count(10)
             ->state(new Sequence(
                 fn ($sequence) => [
                     'type_id' => $documentType->random()->id
@@ -32,22 +40,68 @@ class DocumentsSeeder extends Seeder
             ->for($user->department)
             ->for($user, 'creator')
             ->create();
-
-            $typeFolder = $documentType->where('name', 'Carpeta')->first();
-            $typeFile = $documentType->where('name', 'Archivo')->first();
-
-            $folders = Document::where('type_id', $typeFolder->id)
-            ->where('department_id', $user->department->id)
-            ->get();
-
-            $folders->each(function ($folder) use ($typeFile, $user) {
-                $quantity = rand(2, 4);
-                Document::factory()->count($quantity)
-                ->for($typeFile, 'type')
+            
+            $foldersRoot = $documentsRoot->where('type_id', $typeFolder->id);
+            
+            //level 2
+            $foldersRoot->each(function ($folderRoot) use (
+                $documentType,
+                $typeFolder,
+                $typeFile, 
+                $user
+            ) {
+                $quantity = rand(3, 5);
+                $documentsSecondLevel = Document::factory()->count($quantity)
+                ->state(new Sequence(
+                    fn ($sequence) => [
+                        'type_id' => $documentType->random()->id
+                    ]
+                ))
                 ->for($user->department)
                 ->for($user, 'creator')
-                ->for($folder, 'parent')
+                ->for($folderRoot, 'parent')
                 ->create();
+
+                //level 3
+                $foldersSecondLevel = $documentsSecondLevel->where('type_id', $typeFolder->id);
+
+                $foldersSecondLevel->each(function ($folderSecondLevel) use (
+                    $documentType,
+                    $typeFolder,
+                    $typeFile, 
+                    $user
+                ) {
+                    $quantity = rand(3, 5);
+                    $documentsThreeLevel = Document::factory()->count($quantity)
+                    ->state(new Sequence(
+                        fn ($sequence) => [
+                            'type_id' => $documentType->random()->id
+                        ]
+                    ))
+                    ->for($user->department)
+                    ->for($user, 'creator')
+                    ->for($folderSecondLevel, 'parent')
+                    ->create();
+                    
+                    //level 4
+                    $foldersThreeLevel = $documentsThreeLevel->where('type_id', $typeFolder->id);
+
+                    $foldersThreeLevel->each(function ($folderThreeLevel) use (
+                        $documentType,
+                        $typeFolder,
+                        $typeFile, 
+                        $user
+                    ) {
+                        $quantity = rand(2, 4);
+                        Document::factory()->count($quantity)
+                        ->for($typeFile, 'type')
+                        ->for($user->department)
+                        ->for($user, 'creator')
+                        ->for($folderThreeLevel, 'parent')
+                        ->create();
+                    });
+                
+                });
             });
         });
     }
